8f8b6389de6b2193c8de960a5cd3590805c5371b
Valeria Nemirova <val.pho.35@gmail.com>
2026-01-03 14:19:43 +0300

    changed popup behaviour

--- src/app/views/home/main/main.component.ts
+++ src/app/views/home/main/main.component.ts
@@ -65,7 +65,7 @@ export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
   private popupComponent!: PopupComponent;
 
   ngAfterViewInit(): void {
-    this.popupComponent.open();
+    // this.popupComponent.open();
 
     // const modalRef = this.modalService.open(PopupComponent);
 		// modalRef.componentInstance.data = 'Main component';
